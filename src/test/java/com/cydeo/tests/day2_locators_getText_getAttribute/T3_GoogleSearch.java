343a49a6627576afad496a0de0bd40c48672df4f
Ekrem Meşe <ekremmese1@gmail.com>
2022-12-19 12:26:05 +0300

    New code fragments added.

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_GoogleSearch {
@@ -16,13 +19,26 @@ public class T3_GoogleSearch {
         driver.manage().window().maximize();
 
         //2- Go to: https://google.com
-
         driver.get("https://www.google.com");
 
         //3- Write “apple” in search box
-        //4- Click google search button
+        //4- PRESS ENTER using Keys.ENTER
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple" + Keys.ENTER);
+
         //5- Verify title:
         //Expected: Title should start with “apple” word
+        String expectedInTitle = "apple";
+        String actualTitle = driver.getTitle();
+
+        //verification with if statement
+        if(actualTitle.startsWith(expectedInTitle)){
+            System.out.println("Title verification passed!");
+        } else {
+            System.out.println("Title verification failed!");
+
+        }
 
     }
 }
+
