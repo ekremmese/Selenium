aec38b5d77e812f7eefc397dbd61d77633ad0c6f
Ekrem Meşe <ekremmese1@gmail.com>
2023-01-16 20:16:33 +0300

    New code fragments added.

---
+++ src/test/java/com/cydeo/tests/day5_/TestNG_Intro.java
@@ -0,0 +1,55 @@
+package com.cydeo.tests.day5_;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestNG_Intro {
+
+
+    @Test()
+    public void test1() {
+        System.out.println( "Test1 is running....");
+
+        //ASSERT EQUALS:compate 2 of the same things
+
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+    @Test()
+    public  void test2(){
+        System.out.println( "Test2 is running....");
+
+    }
+
+
+
+
+    @BeforeClass
+    public  void setupMethod(){
+        System.out.println("--------->BeforeClass is running!");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("--------> AfterClass is running!");
+    }
+
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("----<BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---->AfterMethod is running!");
+    }
+
+
+
+
+}
