5b05aced25c36e580f4f10e0aa70ccef1b1fbb81
Ekrem Meşe <ekremmese1@gmail.com>
2023-01-18 15:13:28 +0300

    New code fragments added.

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,64 @@
+package com.cydeo.utilities;
+
+/*
+In this class only general utility methods that are not related to some specific page.
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
+public class BrowserUtils {
+
+    /*
+    This method will accept int (int seconds) and execute Thread.sleep for given duration
+     */
+    public static void sleep(int second){
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e){
+
+        }
+    }
+
+    /*
+    TC #2: Create utility method
+1. Create a new class called BrowserUtils
+2. Create a method to make Task1 logic re-usable
+3. When method is called, it should switch window and verify title.
+Method info:
+• Name: switchWindowAndVerify
+• Return type: void
+• Arg1: WebDriver
+• Arg2: String expectedInUrl
+• Arg3: String expectedTitle
+     */
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle){
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().equals(expectedInUrl)) {
+                break;
+            }
+
+        }
+
+        //5. Assert: Title contains expectedInTitle
+        String actualTitle = driver.getTitle();
+        String expectedInTitle = expectedTitle;
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+    }
+
+}
