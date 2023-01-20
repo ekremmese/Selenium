5de332736152fa0416d5f0483ab4ce3a77a186cd
Ekrem Meşe <ekremmese1@gmail.com>
2023-01-20 21:58:55 +0300

    New code fragments added.

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRMLogin.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRMLogin.java
@@ -1,18 +1,14 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.CRM_Utilities;
-import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class T3_CRMLogin extends TestBase {
 
-public class T3_CRMLogin {
-    WebDriver driver;
     /*
     TC #3: Login scenario
 1. Create new test and make set ups
@@ -27,14 +23,7 @@ helpdesk1@cybertekschool.com UserUser
 Helpdesk2@cybertekschool.com UserUser
      */
 
-    @BeforeMethod
-    public void setupMethod(){
-        //TC #3: Login scenario
-        //1. Create new test and make set ups
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
+
 
     @Test
     public void crm_login_test(){
